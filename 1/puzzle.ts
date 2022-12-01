0e198c722b0a5fd1ce11a8da2769dd058381a8f9
Matt Lehrer <lehrerm@gmail.com>
2022-12-01 20:34:06 +0100

    day 1 complete

--- 1/puzzle.ts
+++ 1/puzzle.ts
@@ -4,7 +4,6 @@ const input = readFileSync('./puzzle_input.txt', 'utf-8');
 
 // split by new line
 const lines = input.split('\n');
-console.log(lines.length);
 
 // create array of each elf's calories
 const caloriesByElf: number[] = [];
@@ -17,7 +16,10 @@ for (let i = 0; i < lines.length; i++) {
 		currentElf += parseInt(lines[i]);
 	}
 }
+// no new line at end of file
 caloriesByElf.push(currentElf);
 
-console.log(caloriesByElf.length);
-console.log(caloriesByElf.sort((a,b)=>a-b).reverse()[0]);
+console.log('Number of elves:', caloriesByElf.length);
+const caloriesByElfSorted = caloriesByElf.sort((a, b) => a - b).reverse();
+console.log('Calories carried by top elf: ', caloriesByElfSorted[0]);
+console.log('Calories carried by top 3 elves: ', caloriesByElfSorted.slice(0, 3).reduce((a, b) => a + b, 0));
