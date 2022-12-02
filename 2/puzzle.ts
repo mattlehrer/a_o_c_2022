3a754253382b49a3a649eb7c26ca915458cbe8d3
Matt Lehrer <lehrerm@gmail.com>
2022-12-02 10:00:25 +0100

    part 2

--- 2/puzzle.ts
+++ 2/puzzle.ts
@@ -41,14 +41,52 @@ const outcome = (opponent: opponentThrow, player: keyof typeof pointsForThrow): 
 	throw new Error('Invalid');
 }
 
+type part2 = {
+	X: 'lose',
+	Y: 'draw',
+	Z: 'win',
+}
+
+const playerShouldThrow = (opponent: opponentThrow, desiredOutcome: keyof part2): keyof typeof pointsForThrow => {
+	switch (opponent) {
+		case 'A': { // Rock
+			if (desiredOutcome === 'X') return 'Z'; // throw scissors to lose
+			if (desiredOutcome === 'Y') return 'X'; // throw rock to draw
+			if (desiredOutcome === 'Z') return 'Y'; // throw paper to win
+			break
+		}
+		case 'B': { // Paper
+			if (desiredOutcome === 'X') return 'X'; // throw rock to lose
+			if (desiredOutcome === 'Y') return 'Y'; // throw paper to draw
+			if (desiredOutcome === 'Z') return 'Z'; // throw scissors to win
+			break
+		}
+		case 'C': { // Scissors
+			if (desiredOutcome === 'X') return 'Y'; // throw paper to lose
+			if (desiredOutcome === 'Y') return 'Z'; // throw scissors to draw
+			if (desiredOutcome === 'Z') return 'X'; // throw rock to win
+			break
+		}
+	}
+	throw new Error('Invalid');
+}
+
 const input = readFileSync('./input.txt', 'utf-8');
 
 // split by new line
 const lines = input.split('\n');
 
+// const score = lines.map(line => {
+// 	const [opponent, player] = line.split(' ');
+// 	if (!opponent || !player) return 0;
+// 	return pointsForThrow[player] + pointsForOutcome[outcome(opponent as opponentThrow, player as keyof typeof pointsForThrow)];
+// }).reduce((a, b) => a + b, 0);
+
+// part 2
 const score = lines.map(line => {
-	const [opponent, player] = line.split(' ');
-	if (!opponent || !player) return 0;
+	const [opponent, desiredOutcome] = line.split(' ');
+	if (!opponent || !desiredOutcome) return 0;
+	const player = playerShouldThrow(opponent as opponentThrow, desiredOutcome as keyof part2);
 	return pointsForThrow[player] + pointsForOutcome[outcome(opponent as opponentThrow, player as keyof typeof pointsForThrow)];
 }).reduce((a, b) => a + b, 0);
 
