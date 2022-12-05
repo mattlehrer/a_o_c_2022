3c570d4488108e467f6fd840a6f0a7b1706b7a10
Matt Lehrer <lehrerm@gmail.com>
2022-12-05 10:55:27 +0100

    part 2

--- 5/puzzle.ts
+++ 5/puzzle.ts
@@ -18,29 +18,18 @@ const stackNumbersInput: string = stacksInput[stacksInput.length - 1];
 const stackNumbersArray: string[] = stackNumbersInput.split('').filter((_, i) => i % 4 === 1);
 const numberOfStacks = parseInt(stackNumbersArray[stackNumbersArray.length - 1]);
 
+part1: {
+
 const stacks: string[][] = [];
 for (let n = 0; n < numberOfStacks; n++) {
 	stacks.push([]);
 }
 
-function parseStackInput(stackInput: string): string[] {
-	// split it and return the 2nd and then every 4th element
-	// this will return ' ' for empty stack spots
-	return stackInput.split('').filter((_, i) => i % 4 === 1);
-}
-
-for (let j = stacksInput.length-2; j >=0 ; j--) {
+for (let j = stacksInput.length - 2; j >= 0; j--) {
 	const row = parseStackInput(stacksInput[j]);
 	for (let k = 0; k < row.length; k++) {
 		if (row[k] !== ' ') stacks[k].push(row[k]);
 	}
-}
-
-function parseMoveInput(moveInput: string): [crates: number, from: number, to: number] {
-	const parsed = moveInput.match(/(\d+)/gm)
-	if (parsed?.length !== 3 || parsed.every(n => typeof n === 'number')) throw new Error('Invalid move input');
-	const output = parsed.map((n) => parseInt(n));
-	return output as [number, number, number];
 }
 
 for (const move of movesInput) {
@@ -55,4 +44,49 @@ for (const move of movesInput) {
 }
 
 const part1 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
-console.log(part1);
+console.log('part 1: ', part1);
+}
+
+part2: {
+	const stacks: string[][] = [];
+for (let n = 0; n < numberOfStacks; n++) {
+	stacks.push([]);
+}
+
+for (let j = stacksInput.length - 2; j >= 0; j--) {
+	const row = parseStackInput(stacksInput[j]);
+	for (let k = 0; k < row.length; k++) {
+		if (row[k] !== ' ') stacks[k].push(row[k]);
+	}
+}
+
+for (const move of movesInput) {
+	if (!move) continue;
+	const [crates, fromNatural, toNatural] = parseMoveInput(move);
+	const from = fromNatural - 1;
+	const to = toNatural - 1;
+	const tempStack: string[] = [];
+	for (let n = 0; n < crates; n++) {
+		if (stacks[from].length === 0) throw new Error('Invalid move: no crates to move');
+		tempStack.push(stacks[from].pop()!);
+	}
+	tempStack.reverse();
+	stacks[to].push(...tempStack);
+}
+
+const part2 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
+console.log('part 2: ', part2);
+}
+
+function parseStackInput(stackInput: string): string[] {
+	// split it and return the 2nd and then every 4th element
+	// this will return ' ' for empty stack spots
+	return stackInput.split('').filter((_, i) => i % 4 === 1);
+}
+
+function parseMoveInput(moveInput: string): [crates: number, from: number, to: number] {
+	const parsed = moveInput.match(/(\d+)/gm)
+	if (parsed?.length !== 3 || parsed.every(n => typeof n === 'number')) throw new Error('Invalid move input');
+	const output = parsed.map((n) => parseInt(n));
+	return output as [number, number, number];
+}
