805fb8cf907a3ddfc10f6ef701a96a987acd2cc8
Matt Lehrer <lehrerm@gmail.com>
2022-12-05 10:43:50 +0100

    part 1

---
+++ 5/puzzle.ts
@@ -0,0 +1,58 @@
+import { readFileSync } from 'node:fs';
+
+const input = readFileSync('./input.txt', 'utf-8');
+
+const lines = input.split('\n');
+
+const stacksInput: string[] = [];
+
+let i = 0;
+while (lines[i] !== '') {
+	stacksInput.push(lines[i]);
+	i++;
+}
+
+const movesInput = lines.splice(i+1);
+
+const stackNumbersInput: string = stacksInput[stacksInput.length - 1];
+const stackNumbersArray: string[] = stackNumbersInput.split('').filter((_, i) => i % 4 === 1);
+const numberOfStacks = parseInt(stackNumbersArray[stackNumbersArray.length - 1]);
+
+const stacks: string[][] = [];
+for (let n = 0; n < numberOfStacks; n++) {
+	stacks.push([]);
+}
+
+function parseStackInput(stackInput: string): string[] {
+	// split it and return the 2nd and then every 4th element
+	// this will return ' ' for empty stack spots
+	return stackInput.split('').filter((_, i) => i % 4 === 1);
+}
+
+for (let j = stacksInput.length-2; j >=0 ; j--) {
+	const row = parseStackInput(stacksInput[j]);
+	for (let k = 0; k < row.length; k++) {
+		if (row[k] !== ' ') stacks[k].push(row[k]);
+	}
+}
+
+function parseMoveInput(moveInput: string): [crates: number, from: number, to: number] {
+	const parsed = moveInput.match(/(\d+)/gm)
+	if (parsed?.length !== 3 || parsed.every(n => typeof n === 'number')) throw new Error('Invalid move input');
+	const output = parsed.map((n) => parseInt(n));
+	return output as [number, number, number];
+}
+
+for (const move of movesInput) {
+	if (!move) continue;
+	const [crates, fromNatural, toNatural] = parseMoveInput(move);
+	const from = fromNatural - 1;
+	const to = toNatural - 1;
+	for (let n = 0; n < crates; n++) {
+		if (stacks[from].length === 0) throw new Error('Invalid move: no crates to move');
+		stacks[to].push(stacks[from].pop()!);
+	}
+}
+
+const part1 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
+console.log(part1);
