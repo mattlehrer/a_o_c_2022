b9b0182723dc9837aef0472eece13083e64d890e
Matt Lehrer <lehrerm@gmail.com>
2022-12-05 10:58:44 +0100

    format

--- 5/puzzle.ts
+++ 5/puzzle.ts
@@ -20,62 +20,62 @@ const numberOfStacks = parseInt(stackNumbersArray[stackNumbersArray.length - 1])
 
 part1: {
 
-const stacks: string[][] = [];
-for (let n = 0; n < numberOfStacks; n++) {
-	stacks.push([]);
-}
-
-for (let j = stacksInput.length - 2; j >= 0; j--) {
-	const row = parseStackInput(stacksInput[j]);
-	for (let k = 0; k < row.length; k++) {
-		if (row[k] !== ' ') stacks[k].push(row[k]);
+	const stacks: string[][] = [];
+	for (let n = 0; n < numberOfStacks; n++) {
+		stacks.push([]);
 	}
-}
 
-for (const move of movesInput) {
-	if (!move) continue;
-	const [crates, fromNatural, toNatural] = parseMoveInput(move);
-	const from = fromNatural - 1;
-	const to = toNatural - 1;
-	for (let n = 0; n < crates; n++) {
-		if (stacks[from].length === 0) throw new Error('Invalid move: no crates to move');
-		stacks[to].push(stacks[from].pop()!);
+	for (let j = stacksInput.length - 2; j >= 0; j--) {
+		const row = parseStackInput(stacksInput[j]);
+		for (let k = 0; k < row.length; k++) {
+			if (row[k] !== ' ') stacks[k].push(row[k]);
+		}
 	}
-}
 
-const part1 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
-console.log('part 1: ', part1);
+	for (const move of movesInput) {
+		if (!move) continue;
+		const [crates, fromNatural, toNatural] = parseMoveInput(move);
+		const from = fromNatural - 1;
+		const to = toNatural - 1;
+		for (let n = 0; n < crates; n++) {
+			if (stacks[from].length === 0) throw new Error('Invalid move: no crates to move');
+			stacks[to].push(stacks[from].pop()!);
+		}
+	}
+
+	const part1 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
+	console.log('part 1: ', part1);
 }
 
 part2: {
 	const stacks: string[][] = [];
-for (let n = 0; n < numberOfStacks; n++) {
-	stacks.push([]);
-}
-
-for (let j = stacksInput.length - 2; j >= 0; j--) {
-	const row = parseStackInput(stacksInput[j]);
-	for (let k = 0; k < row.length; k++) {
-		if (row[k] !== ' ') stacks[k].push(row[k]);
+	for (let n = 0; n < numberOfStacks; n++) {
+		stacks.push([]);
 	}
-}
 
-for (const move of movesInput) {
-	if (!move) continue;
-	const [crates, fromNatural, toNatural] = parseMoveInput(move);
-	const from = fromNatural - 1;
-	const to = toNatural - 1;
-	const tempStack: string[] = [];
-	for (let n = 0; n < crates; n++) {
-		if (stacks[from].length === 0) throw new Error('Invalid move: no crates to move');
-		tempStack.push(stacks[from].pop()!);
+	for (let j = stacksInput.length - 2; j >= 0; j--) {
+		const row = parseStackInput(stacksInput[j]);
+		for (let k = 0; k < row.length; k++) {
+			if (row[k] !== ' ') stacks[k].push(row[k]);
+		}
 	}
-	tempStack.reverse();
-	stacks[to].push(...tempStack);
-}
 
-const part2 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
-console.log('part 2: ', part2);
+	for (const move of movesInput) {
+		if (!move) continue;
+		const [crates, fromNatural, toNatural] = parseMoveInput(move);
+		const from = fromNatural - 1;
+		const to = toNatural - 1;
+		const tempStack: string[] = [];
+		for (let n = 0; n < crates; n++) {
+			if (stacks[from].length === 0) throw new Error('Invalid move: no crates to move');
+			tempStack.push(stacks[from].pop()!);
+		}
+		tempStack.reverse();
+		stacks[to].push(...tempStack);
+	}
+
+	const part2 = stacks.reduce((acc, stack) => acc + stack[stack.length - 1], '');
+	console.log('part 2: ', part2);
 }
 
 function parseStackInput(stackInput: string): string[] {
