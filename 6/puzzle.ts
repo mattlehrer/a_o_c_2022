a93f3f7c6fcf19d5e1a0b0527f2516740ba2ebdc
Matt Lehrer <lehrerm@gmail.com>
2022-12-06 13:47:33 +0100

    part 2

--- 6/puzzle.ts
+++ 6/puzzle.ts
@@ -6,14 +6,20 @@ const lines = input.split('\n');
 
 for (const line of lines) {
 	const chars = line.split('');
-	let i = 4;
+	detectMarker(chars);
+	detectMarker(chars, 14, 2);
+}
+
+function detectMarker(chars: string[], length: number = 4, part: number = 1) {
+	let i = length;
 	while (i < chars.length) {
-		const slice = chars.slice(i-4, i);
+		const slice = chars.slice(i - length, i);
 		const test = new Set(slice);
-		if (test.size === 4) {
-			console.log(`Found it at ${i}`);
+		if (test.size === length) {
+			console.log(`Found part ${part} at ${i}`);
 			break;
 		}
 		i++;
 	}
 }
+
