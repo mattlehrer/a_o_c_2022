0e021459de8ef2d126c06f8f0556a11cf4cf2855
Matt Lehrer <lehrerm@gmail.com>
2022-12-11 10:08:57 +0100

    wip

--- 11/puzzle.ts
+++ 11/puzzle.ts
@@ -1,92 +1,106 @@
 import { readFileSync } from 'node:fs';
 
-const input = readFileSync('./input.txt', 'utf-8');
+const input = readFileSync('./test.txt', 'utf-8');
 
 const lines = input.split('\n');
 
-let currentMonkey = 0;
-const monkeys: Record<
-	number,
-	{
-		items: number[];
-		operations: { type: '+' | '*' | '**'; amount: number };
-		test: number;
-		onTrue: number;
-		onFalse: number;
-		inspections: number;
-	}
-> = {};
+function parseInput(input: string[]): Monkeys {
+	let currentMonkey = 0;
+	const monkeys: Monkeys = {};
 
-const WORRY_DROP = 3;
-
-for (const pretrim of lines) {
-	const line = pretrim.trim();
-	if (line === '') {
-		continue;
-	} else if (line.startsWith('Monkey ')) {
-		currentMonkey = Number(line.slice(7, 8));
-		monkeys[currentMonkey] = {
-			items: [],
-			operations: { type: '+', amount: 0 },
-			test: 0,
-			onTrue: 0,
-			onFalse: 0,
-			inspections: 0,
-		};
-	} else if (line.startsWith('Starting items: ')) {
-		monkeys[currentMonkey].items = [
-			...monkeys[currentMonkey].items,
-			...line.slice(16).split(', ').map(Number),
-		];
-	} else if (line.startsWith('Operation: new = old + ')) {
-		monkeys[currentMonkey].operations = {
-			type: '+',
-			amount: Number(line.slice(23)),
-		};
-	} else if (line.startsWith('Operation: new = old * ')) {
-		if (line.slice(23) === 'old') {
-			monkeys[currentMonkey].operations = {
-				type: '**',
-				amount: 2,
+	for (const pretrim of input) {
+		const line = pretrim.trim();
+		if (line === '') {
+			continue;
+		} else if (line.startsWith('Monkey ')) {
+			currentMonkey = Number(line.slice(7, 8));
+			monkeys[currentMonkey] = {
+				items: [],
+				operations: { type: '+', amount: 0 },
+				test: 0,
+				onTrue: -1,
+				onFalse: -1,
+				inspections: 0,
 			};
-		} else {
+		} else if (line.startsWith('Starting items: ')) {
+			monkeys[currentMonkey].items = [
+				...monkeys[currentMonkey].items,
+				...line.slice(16).split(', ').map(Number),
+			];
+		} else if (line.startsWith('Operation: new = old + ')) {
 			monkeys[currentMonkey].operations = {
-				type: '*',
+				type: '+',
 				amount: Number(line.slice(23)),
 			};
-		}
-	} else if (line.startsWith('Test: divisible by ')) {
-		monkeys[currentMonkey].test = Number(line.slice(19));
-	} else if (line.startsWith('If true: throw to monkey ')) {
-		monkeys[currentMonkey].onTrue = Number(line.slice(25));
-	} else if (line.startsWith('If false: throw to monkey ')) {
-		monkeys[currentMonkey].onFalse = Number(line.slice(26));
-	} else {
-		throw new Error('Unknown line: ' + line);
-	}
-}
-
-for (let round = 1; round < 21; round++) {
-	for (const monkey of Object.values(monkeys)) {
-		while (monkey.items.length > 0) {
-			const item = monkey.items.shift()!;
-			let worryLevel: number;
-			if (monkey.operations.type === '+') {
-				worryLevel = Math.floor((item + monkey.operations.amount) / WORRY_DROP);
-			} else if (monkey.operations.type === '*') {
-				worryLevel = Math.floor((item * monkey.operations.amount) / WORRY_DROP);
+		} else if (line.startsWith('Operation: new = old * ')) {
+			if (line.slice(23) === 'old') {
+				monkeys[currentMonkey].operations = {
+					type: '**',
+					amount: 2,
+				};
 			} else {
-				worryLevel = Math.floor(item ** monkey.operations.amount / WORRY_DROP);
+				monkeys[currentMonkey].operations = {
+					type: '*',
+					amount: Number(line.slice(23)),
+				};
 			}
-			if (worryLevel % monkey.test === 0) {
-				monkeys[monkey.onTrue].items.push(worryLevel);
-			} else {
-				monkeys[monkey.onFalse].items.push(worryLevel);
-			}
-			monkey.inspections++;
+		} else if (line.startsWith('Test: divisible by ')) {
+			monkeys[currentMonkey].test = Number(line.slice(19));
+		} else if (line.startsWith('If true: throw to monkey ')) {
+			monkeys[currentMonkey].onTrue = Number(line.slice(25));
+		} else if (line.startsWith('If false: throw to monkey ')) {
+			monkeys[currentMonkey].onFalse = Number(line.slice(26));
+		} else {
+			throw new Error('Unknown line: ' + line);
 		}
 	}
+	return monkeys;
 }
 
-const sorted = Object.values(monkeys).sort((a, b) => b.inspections - a.inspections);
-console.log(sorted.slice(0, 2).reduce((product, curr) => product * curr.inspections, 1));
+function monkeyBusiness(monkeys: Monkeys, worryDrop: number = 3, rounds: number = 20): number {
+	for (let round = 0; round < rounds; round++) {
+		for (const monkey of Object.values(monkeys)) {
+			while (monkey.items.length > 0) {
+				const item = monkey.items.shift();
+				if (!item) throw new Error('No item!');
+				let worryLevel: number;
+				if (monkey.operations.type === '+') {
+					worryLevel = Math.floor((item + monkey.operations.amount) / worryDrop);
+				} else if (monkey.operations.type === '*') {
+					worryLevel = Math.floor((item * monkey.operations.amount) / worryDrop);
+				} else {
+					// the problem is here --- can exceed Number.MAX_SAFE_INTEGER
+					worryLevel = Math.floor(item ** monkey.operations.amount / worryDrop);
+				}
+				if (worryLevel % monkey.test === 0) {
+					monkeys[monkey.onTrue].items.push(worryLevel);
+				} else {
+					monkeys[monkey.onFalse].items.push(worryLevel);
+				}
+				monkey.inspections++;
+			}
+		}
+	}
+	console.log(
+		Object.values(monkeys)
+			.map((m) => m.inspections)
+			.join(', '),
+	);
+
+	const sorted = Object.values(monkeys).sort((a, b) => b.inspections - a.inspections);
+	return sorted.slice(0, 2).reduce((product, curr) => product * curr.inspections, 1);
+}
+
+console.log('Part 1:', monkeyBusiness(parseInput(lines), 3, 20));
+console.log('Part 2:', monkeyBusiness(parseInput(lines), 1, 10000));
+
+type Monkey = {
+	items: number[];
+	operations: { type: '+' | '*' | '**'; amount: number };
+	test: number;
+	onTrue: number;
+	onFalse: number;
+	inspections: number;
+};
+
+type Monkeys = Record<number, Monkey>;
