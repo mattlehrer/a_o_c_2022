b8ee1e55270b21c5b40a8958b49185182fa5fb7d
Matt Lehrer <lehrerm@gmail.com>
2022-12-15 14:45:30 +0100

    wip

--- 15/puzzle.ts
+++ 15/puzzle.ts
@@ -14,30 +14,62 @@ for (const line of lines) {
 	locations.push([parseInt(x), parseInt(y), parseInt(bx), parseInt(by)]);
 }
 
-const targetRowHits = new Set<number>();
-const targetRow = 2000000;
-for (const [x, y, bx, by] of locations) {
-	const d = getDistance([x, y], [bx, by]);
-	if (y - d <= targetRow && y + d >= targetRow) {
-		const yDiff = Math.abs(y - targetRow);
-		const xRange = range(x - (d - yDiff), x + (d - yDiff) + 1);
-		// console.log({ x, y, bx, by, d, xRange });
-		xRange.forEach((i) => targetRowHits.add(i));
-	}
-}
-for (const [, , bx, by] of locations) {
-	if (by === targetRow) {
-		targetRowHits.delete(bx);
-	}
-}
+// const targetRowHits = new Set<number>();
+// const targetRow = 2000000;
+// for (const [x, y, bx, by] of locations) {
+// 	const d = getDistance([x, y], [bx, by]);
+// 	if (y - d <= targetRow && y + d >= targetRow) {
+// 		const yDiff = Math.abs(y - targetRow);
+// 		const xRange = range(x - (d - yDiff), x + (d - yDiff) + 1);
+// 		// console.log({ x, y, bx, by, d, xRange });
+// 		xRange.forEach((i) => targetRowHits.add(i));
+// 	}
+// }
+// for (const [, , bx, by] of locations) {
+// 	if (by === targetRow) {
+// 		targetRowHits.delete(bx);
+// 	}
+// }
 
-console.log('Part 1', targetRowHits.size);
+// console.log('Part 1', targetRowHits.size);
 console.timeEnd('Part 1');
 
-// console.time('Part 2');
+console.time('Part 2');
 
-// console.log('Part 2', '');
-// console.timeEnd('Part 2');
+const MIN = 0;
+const MAX = 4_000_000;
+// const MAX = 20;
+const TUNING_X_MULTIPLIER = 4_000_000;
+
+for (let i = MIN; i <= MAX; i++) {
+	const row = new Set<number>();
+	for (const [x, y, bx, by] of locations) {
+		const d = getDistance([x, y], [bx, by]);
+		if (y - d <= i && y + d >= i) {
+			const yDiff = Math.abs(y - i);
+			const xRange = range(Math.max(x - (d - yDiff), MIN), Math.min(x + (d - yDiff) + 1, MAX + 1));
+			// console.log({ x, y, bx, by, d, xRange });
+			xRange.forEach((j) => row.add(j));
+		}
+	}
+	for (const [x, y, bx, by] of locations) {
+		if (by === i && bx >= MIN && bx <= MAX) {
+			row.add(bx);
+		}
+		if (y === i && x >= MIN && x <= MAX) {
+			row.add(x);
+		}
+	}
+	if (row.size !== MAX + 1) {
+		const y = i;
+		const x = (MAX * (MAX + 1)) / 2 - [...row].reduce((a, b) => a + b, 0);
+
+		console.log('Part 2', x * TUNING_X_MULTIPLIER + y);
+		break;
+	}
+}
+
+console.timeEnd('Part 2');
 
 type SensorAndBeacon = [number, number, number, number];
 
